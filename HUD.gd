0831dfd521bd0e004fe48a18f14203fdef6d35b5
paulonavesneto <paulonavesneto@gmail.com>
2023-09-10 18:10:30 +0200

    wip: Start splitting Menu and game

--- HUD.gd
+++ HUD.gd
@@ -3,27 +3,6 @@ extends CanvasLayer
 # Notifies Main node that the button as been pressed
 signal start_game
 
-func show_message(text):
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
-
-func show_game_over():
-	show_message("You suck!")
-	# Wait until the MessageTimer has counted down.
-	await $MessageTimer.timeout
-	
-	$Message.text = "Dodge the\nmonsters!"
-	$Message.show()
-	
-	# Make a one-shot timer and wait for it to finish.
-	await get_tree().create_timer(1.5).timeout
-	$Startbutton.show()
-
-func update_score(score):
-	$ScoreLabel.text = str(score)
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -33,11 +12,34 @@ func _ready():
 func _process(delta):
 	pass
 
+func load_menu(text):
+	$ScoreLabel.hide()
+	$Message.text = text
+	$Message.show()
+	
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+
+func show_game_over():
+	show_message("You suck!")
+	# Wait until the MessageTimer has counted down.
+	await $MessageTimer.timeout
+	# Make a one-shot timer and wait for it to finish.
+	await get_tree().create_timer(1.5).timeout
+	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
 
 func _on_message_timer_timeout():
 	$Message.hide()
 
 
 func _on_startbutton_pressed():
-	$Startbutton.hide()
+	$StartButton.hide()
+	$ScoreLabel.show()
 	start_game.emit()
+	get_tree().change_scene_to_file("res://phase_1.tscn")
